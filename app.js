cbdbde67e5bba0f24687c1a1448c8264851364ba
Abdullah Al-Mahfadi <66219679+ab-almahfadi@users.noreply.github.com>
2023-04-28 17:59:10 +0800

    New Commit1

--- app.js
+++ app.js
@@ -88,6 +88,11 @@ app.get("/posts/:postName", async function(req, res){
 
 });
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+} 
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
